0b2f2891218887cab9ea279c93a993d4c02ed55b
Kanch Ruansiripiyakul <digitazine@gmail.com>
2022-11-17 11:31:12 +0700

    Moving Player transform but having but player are near camera

--- Terosoft Bootcamp/Assets/EditorController.cs
+++ Terosoft Bootcamp/Assets/EditorController.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 public class EditorController : MonoBehaviour
 {
     public Camera camera;
+    public GameObject SelectObj;
+    
+    Vector3 Hitpoint;
 
     void Update(){
-        
         RaycastHit hit;
         Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-        
         if (Physics.Raycast(ray, out hit)) {
            Debug.DrawLine(camera.transform.position,hit.point,Color.red);
            Debug.Log("Hit"+hit.point);
            if(hit.collider.GetComponent<PlayerController>() != null){
                 Debug.Log("Name " + hit.collider.GetComponent<PlayerController>().Name);
            }
+
+           if(Input.GetMouseButtonDown(0)){
+            SelectObj = hit.collider.gameObject;
+           }
            
+        }
+        if(SelectObj != null){
+            Hitpoint = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y, camera.nearClipPlane));
+            Debug.Log("HitPoint" + Hitpoint);
+            if(SelectObj.name == "Player"){
+                SelectObj.transform.position = Hitpoint;
+            }
         }
 
     }
