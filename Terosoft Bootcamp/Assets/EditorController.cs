f6c0ad4fb47e6a86e45ced3cc024ae0ec264c0cf
Kanch Ruansiripiyakul <digitazine@gmail.com>
2022-11-17 12:51:06 +0700

    Move object when click

--- Terosoft Bootcamp/Assets/EditorController.cs
+++ Terosoft Bootcamp/Assets/EditorController.cs
@@ -8,28 +8,56 @@ public class EditorController : MonoBehaviour
     public GameObject SelectObj;
     
     Vector3 Hitpoint;
+    Vector3 MousePoint;
 
     void Update(){
         RaycastHit hit;
         Ray ray = camera.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hit)) {
+
+           if(hit.collider.tag == "ground"){
+                MousePoint = hit.point;
+           }
+
            Debug.DrawLine(camera.transform.position,hit.point,Color.red);
            Debug.Log("Hit"+hit.point);
+
            if(hit.collider.GetComponent<PlayerController>() != null){
                 Debug.Log("Name " + hit.collider.GetComponent<PlayerController>().Name);
            }
 
            if(Input.GetMouseButtonDown(0)){
-            SelectObj = hit.collider.gameObject;
+                if(SelectObj != null){
+                    SelectObj.GetComponent<Collider>().enabled = true;
+                }
+                SelectObj = hit.collider.gameObject;
            }
-           
         }
+
+        if(Input.GetMouseButtonDown(1)){
+            if(SelectObj != null){
+                SelectObj.GetComponent<Collider>().enabled = true;
+                SelectObj = null;
+            }
+        }
+
         if(SelectObj != null){
             Hitpoint = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y, camera.nearClipPlane));
             Debug.Log("HitPoint" + Hitpoint);
+
+            if(SelectObj.tag == "movable"){
+                SelectObj.GetComponent<Collider>().enabled = false;
+            }
+            
+
+            if(SelectObj.tag == "movable"){
+                SelectObj.transform.position = MousePoint;
+            }
+            /*
             if(SelectObj.name == "Player"){
                 SelectObj.transform.position = Hitpoint;
             }
+            */
         }
 
     }
